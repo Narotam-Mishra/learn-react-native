68796704ea830f48f5e2c341a8c241f3533e262c
Narotam-Mishra <narotam21jan@gmail.com>
2026-02-23 23:02:48 +0530

    react-native-imp-react-concepts

--- module-1_Intro_to_React_Native/hello-world/App.js
+++ module-1_Intro_to_React_Native/hello-world/App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = "React Native";
+  const age = 20;
+  const str = `Hello ${name} with ${age} years of dominance`;
+
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
+      <Text>{str}</Text>
       <StatusBar style="auto" />
     </View>
   );
