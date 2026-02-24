af5dd2a7f1718f46a9bf8720cb4c84c5289b0c95
Narotam-Mishra <narotam21jan@gmail.com>
2026-02-24 23:42:47 +0530

    react-native-Button_component

--- module-1_Intro_to_React_Native/hello-world/App.js
+++ module-1_Intro_to_React_Native/hello-world/App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log("Button pressed!!");
+  }
+
   const name = "React Native";
   const age = 20;
   const str = `Hello ${name} with ${age} years of dominance`;
 
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
-      </View>
+      <Button color={"red"} title="Submit" onPress={handlePress}></Button>
     </View>
   );
 }
@@ -23,23 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  row: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  row:{
-    flexDirection: "row",
-  },
-  box:{
-    width: 100,
-    height: 100,
-    backgroundColor: "maroon",
-    margin: 5,
   }
 });
